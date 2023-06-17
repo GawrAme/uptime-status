5ee32cc2e886a110ed2cf89ce0112c182b304346
GawrAme <97426017+GawrAme@users.noreply.github.com>
2023-06-17 09:40:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: '站点状态监控---极客青年',
+  SiteName: 'Xray Monitoring',
 
   // 站点链接
   SiteUrl: '/',
@@ -10,7 +10,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两种 Api Key
   ApiKeys: [
-    'ur894242-90d1eb90acc82624bcaed27a'
+    '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'
   ],
 
   // 是否显示监测站点的链接
